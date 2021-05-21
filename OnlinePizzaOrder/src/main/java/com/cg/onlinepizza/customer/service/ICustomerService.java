26175520b1bb0172acb7e7414f4c438999d3fd7f
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-05-21 14:19:51 +0530

    Update ICustomerService.java

--- OnlinePizzaOrder/src/main/java/com/cg/onlinepizza/customer/service/ICustomerService.java
+++ OnlinePizzaOrder/src/main/java/com/cg/onlinepizza/customer/service/ICustomerService.java
@@ -1,18 +1,72 @@
-package com.cg.onlinepizza.customer.service;
+//package com.cg.onlinepizza.customer.service;
 
+//import java.util.List;
+
+//import com.cg.onlinepizza.customer.dto.Customer;
+//import com.cg.onlinepizza.exceptions.CustomerIdNotFoundException;
+
+//public interface ICustomerService {
+//	Customer addCustomer(Customer customer);
+//
+//	Customer updateCustomer(Customer customer);
+//
+//	Customer deleteCustomer(int customerId) throws CustomerIdNotFoundException;
+
+//	List<Customer> viewCustomers();
+
+//	Customer viewCustomer(int customerId) throws CustomerIdNotFoundException;
+//}
+
+package com.cg.onlinepizza.customer.service;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.onlinepizza.customer.dto.Customer;
 import com.cg.onlinepizza.exceptions.CustomerIdNotFoundException;
+import com.cg.onlinepizza.customer.dao.*;
 
-public interface ICustomerService {
-	Customer addCustomer(Customer customer);
 
-	Customer updateCustomer(Customer customer);
+@Service
+public class ICustomerService {
+	@Autowired
+	ICustomerRepository ICustomerRepository;
+	public Customer addCustomer(Customer customer) {
+		return ICustomerRepository.save(customer);
+	}
 
-	Customer deleteCustomer(int customerId) throws CustomerIdNotFoundException;
+	public Customer updateCustomer(Customer customer) {
+		return ICustomerRepository.save(customer);
+	}
 
-	List<Customer> viewCustomers();
+	public void deleteCustomer(int customerId) throws CustomerIdNotFoundException{
+		try {
+			ICustomerRepository.deleteById(customerId);
+		}
+		catch(Exception e) {
+			throw new CustomerIdNotFoundException("Customer Id not present in table");
+		}
+	}
+	
+	public Customer viewCustomer1(int customerId) throws CustomerIdNotFoundException{
+		try {
+			return ICustomerRepository.findById(customerId).get();
+		}
+		catch(Exception e) {
+			throw new CustomerIdNotFoundException("Customer Id not present in table");
+		}
+	}
 
-	Customer viewCustomer(int customerId) throws CustomerIdNotFoundException;
+	public List<Customer> viewCustomers(){
+		List<Customer> customer = new ArrayList<Customer>();
+		ICustomerRepository.findAll().forEach(customer1 -> customer.add(customer1));
+		return customer;
+	}
+
 }
+
+
+
+
